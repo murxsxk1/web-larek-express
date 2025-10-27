19555a3bb47baeb4ec538915f7eeb48ada66caa6
Danila <novash.danila@yandex.by>
2025-10-27 18:57:29 +0300

    feat: добавлены фолбеки для конфига

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -7,8 +7,8 @@ if (!process.env.PORT || !process.env.DB_ADDRESS) {
 }
 
 const config = {
-  PORT: process.env.PORT,
-  DB_ADDRESS: process.env.DB_ADDRESS,
+  PORT: process.env.PORT || '3000',
+  DB_ADDRESS: process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/weblarek',
 };
 
 export default config;
